5c06b44ea27d599491f10a06beb3d4b0375ce8b6
Alvaro <alvar01omer@gmail.com>
2021-03-20 16:10:21 -0300

    test

---
+++ portfolios/pages/3d/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="3d"><h1>asd</h1></div>
+</template>
+<script>
+import * as THREE from 'three'
+export default {
+  layout: 'empty',
+}
+</script>
+
+<style>
+div.3d {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background: #000000;
+  margin: 0;
+  padding: 0;
+  overscroll-behavior: none;
+}
+</style>
