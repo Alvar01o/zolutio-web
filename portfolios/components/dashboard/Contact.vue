bc96c10b083d3968e7f6cba2ad8d539bb6f156e9
Alvaro <alvar01omer@gmail.com>
2021-03-20 21:38:01 -0300

    zolutio layout

---
+++ portfolios/components/dashboard/Contact.vue
@@ -0,0 +1,174 @@
+<template>
+  <div class="relative bg-white" id="contact">
+    <div class="lg:absolute lg:inset-0">
+      <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <img
+          class="h-56 w-full object-cover lg:absolute lg:h-full"
+          src="https://images.unsplash.com/photo-1556761175-4b46a572b786?ixlib=rb-1.2.1&ixqx=iuegsqX3S8&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80"
+          alt=""
+        />
+      </div>
+    </div>
+    <div
+      class="relative py-16 px-4 sm:py-24 sm:px-6 lg:px-8 lg:max-w-7xl lg:mx-auto lg:py-32 lg:grid lg:grid-cols-2"
+    >
+      <div class="lg:pr-8">
+        <div class="max-w-md mx-auto sm:max-w-lg lg:mx-0">
+          <h2
+            class="text-3xl text-zgreen font-extrabold tracking-tight sm:text-4xl"
+          >
+            Let's work together
+          </h2>
+          <p class="mt-4 text-lg text-gray-500 sm:mt-3">
+            We’d love to hear from you! Send us a message using the form
+            opposite, or email us. We’d love to hear from you! Send us a message
+            using the form opposite, or email us.
+          </p>
+          <form
+            action="#"
+            method="POST"
+            class="mt-9 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
+          >
+            <div>
+              <label
+                for="first_name"
+                class="block text-sm font-medium text-gray-700"
+                >First name</label
+              >
+              <div class="mt-1">
+                <input
+                  type="text"
+                  name="first_name"
+                  id="first_name"
+                  autocomplete="given-name"
+                  class="block w-full shadow-sm sm:text-sm focus:ring-grape-500 focus:border-grape-500 border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                for="last_name"
+                class="block text-sm font-medium text-gray-700"
+                >Last name</label
+              >
+              <div class="mt-1">
+                <input
+                  type="text"
+                  name="last_name"
+                  id="last_name"
+                  autocomplete="family-name"
+                  class="block w-full shadow-sm sm:text-sm focus:ring-grape-500 focus:border-grape-500 border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+            <div class="sm:col-span-2">
+              <label for="email" class="block text-sm font-medium text-gray-700"
+                >Email</label
+              >
+              <div class="mt-1">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autocomplete="email"
+                  class="block w-full shadow-sm sm:text-sm focus:ring-grape-500 focus:border-grape-500 border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+            <div class="sm:col-span-2">
+              <label
+                for="company"
+                class="block text-sm font-medium text-gray-700"
+                >Company</label
+              >
+              <div class="mt-1">
+                <input
+                  type="text"
+                  name="company"
+                  id="company"
+                  autocomplete="organization"
+                  class="block w-full shadow-sm sm:text-sm focus:ring-grape-500 focus:border-grape-500 border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+            <div class="sm:col-span-2">
+              <div class="flex justify-between">
+                <label
+                  for="phone"
+                  class="block text-sm font-medium text-gray-700"
+                  >Phone</label
+                >
+                <span id="phone_description" class="text-sm text-gray-500"
+                  >Optional</span
+                >
+              </div>
+              <div class="mt-1">
+                <input
+                  type="text"
+                  name="phone"
+                  id="phone"
+                  autocomplete="tel"
+                  aria-describedby="phone_description"
+                  class="block w-full shadow-sm sm:text-sm focus:ring-grape-500 focus:border-grape-500 border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+            <div class="sm:col-span-2">
+              <div class="flex justify-between">
+                <label
+                  for="how_can_we_help"
+                  class="block text-sm font-medium text-gray-700"
+                  >How can we help you?</label
+                >
+                <span
+                  id="how_can_we_help_description"
+                  class="text-sm text-gray-500"
+                  >Max. 500 characters</span
+                >
+              </div>
+              <div class="mt-1">
+                <textarea
+                  id="how_can_we_help"
+                  name="how_can_we_help"
+                  aria-describedby="how_can_we_help_description"
+                  rows="4"
+                  class="block w-full shadow-sm sm:text-sm focus:ring-grape-500 focus:border-grape-500 border-gray-300 rounded-md"
+                ></textarea>
+              </div>
+            </div>
+
+            <div class="sm:col-span-2">
+              <label
+                for="how_did_you_hear_about_us"
+                class="block text-sm font-medium text-gray-700"
+                >How did you hear about us?</label
+              >
+              <div class="mt-1">
+                <input
+                  type="text"
+                  name="how_did_you_hear_about_us"
+                  id="how_did_you_hear_about_us"
+                  class="shadow-sm focus:ring-grape-500 focus:border-grape-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+            <div class="text-right sm:col-span-2">
+              <button
+                type="submit"
+                class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-grape-600 hover:bg-grape-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-grape-500"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+}
+</script>
---
+++ portfolios/components/dashboard/OurStack.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="bg-zgreen-dark">
+    <h2
+      class="text-3xl font-extrabold tracking-tight pt-5 text-center sm:text-4xl text-zgreen"
+    >
+      OUR STACK
+    </h2>
+    <div
+      class="max-w-2xl py-16 px-6 text-right sm:py-24 sm:px-6 lg:px-8 mx-auto"
+    >
+      <div class="mt-6 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-8">
+        <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+          <img
+            class="max-h-12"
+            src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg"
+            alt="Workcation"
+          />
+        </div>
+        <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+          <img
+            class="max-h-12"
+            src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg"
+            alt="Mirage"
+          />
+        </div>
+        <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+          <img
+            class="max-h-12"
+            src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg"
+            alt="Tuple"
+          />
+        </div>
+        <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+          <img
+            class="max-h-12"
+            src="https://tailwindui.com/img/logos/laravel-logo-gray-400.svg"
+            alt="Laravel"
+          />
+        </div>
+        <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+          <img
+            class="max-h-12"
+            src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg"
+            alt="StaticKit"
+          />
+        </div>
+        <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+          <img
+            class="max-h-12"
+            src="https://tailwindui.com/img/logos/statamic-logo-gray-400.svg"
+            alt="Statamic"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'OurStack',
+}
+</script>
